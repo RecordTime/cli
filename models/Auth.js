c94eca45d27ed6ffbac828debd8e66fc2be5b553
ltaoo <litaowork@aliyun.com>
2018-09-07 15:02:19 +0800

    refactor: 优化根据 board 聚合

--- models/Auth.js
+++ models/Auth.js
@@ -9,8 +9,8 @@ const inquirer = require('inquirer');
  * 登录
  */
 module.exports.login = async function login() {
-  
-}
+
+};
 
 /**
  * 注册
@@ -27,12 +27,12 @@ module.exports.register = async function register() {
     const { email } = await inquirer.prompt([{
       type: 'input',
       message: 'Email',
-      name: 'email'
+      name: 'email',
     }]);
     const { password } = await inquirer.prompt([{
       type: 'password',
       message: 'Password',
-      name: 'password'
+      name: 'password',
     }]);
     const user = new AV.User();
     user.setUsername(username);
@@ -43,7 +43,7 @@ module.exports.register = async function register() {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 program
   .command('register', '注册用户')
@@ -60,14 +60,14 @@ program
 program
   .command('tasks')
   .description('列出当前未完成任务')
-  .option("-s, --setup_mode [mode]", "Which setup mode to use")
+  .option('-s, --setup_mode [mode]', 'Which setup mode to use')
   .option('-a, --all', '获取所有任务')
   .option('-c, --complete', '获取已完成任务')
   /**
    * @param {Commander | String} env - tasks 后面如果存在 [env]，这里就是对应的值，否则就是 Commander
    * @param {Options} - options 如果存在 [env]，就是单个 option 的值，否则就是 undefined
    */
-  .action(async function(env, options) {
+  .action(async (env, options) => {
     const tasks = await fetchTasks();
     console.log(tasks);
   });
--- utils/index.js
+++ utils/index.js
@@ -1,18 +1,15 @@
-module.exports.format = function format(task) {
+function format(task) {
   return {
     _id: task.id,
     createdAt: task.createdAt,
     updatedAt: task.updatedAt,
-    ...task.attributes,
-    description: task.attributes.content,
     // 创建时间
     _timestamp: new Date(task.createdAt).valueOf(),
     _isTask: true,
-    isComplete: false,
-    // 类别
-    boards: ['My Board'],
+    ...task.attributes,
   };
 }
+module.exports.format = format;
 /**
  * 从 AV 中查询到的结果转换为列表
  * @param {Object} objects - 类数组对象，多条记录
@@ -24,4 +21,4 @@ module.exports.getItem = function getItem(objects) {
     prev[id] = format(task);
     return prev;
   }, {});
-}
+};
