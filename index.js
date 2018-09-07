a59ff361fd4b2bc3d502b642eb7a2533ae9043c1
ltaoo <litaowork@aliyun.com>
2018-09-07 14:03:57 +0800

    feat: 判断登录状态并提示登录

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const core = require('./lib');
 
 const recordTimeCLI = (input, flags) => {
   // 先检查是否登录
-  core.login();
+  if (!core._storage.isLogin) {
+    return core.login();
+  }
   if (flags.task) {
     return core.createTask(input);
   }
