e0236ebf2e42cf06cd1c7a9c450bd5c021349a96
ltaoo <litaowork@aliyun.com>
2018-09-07 21:48:29 +0800

    chore: 默认读取本地文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+        "no-underscore-dangle": 0,
+        "class-methods-use-this": 0
+    }
 };
---
+++ common/constants.js
@@ -0,0 +1,12 @@
+/**
+ * @file 一些常量
+ * @author ltaoo
+ */
+module.exports = {
+  APP_DIR_NAME: '.recordtime',
+  STORAGE_DIR_NAME: 'storage',
+  ARCHIVE_DIR_NAME: 'archive',
+  TEMP_DIR_NAME: '.temp',
+  ARCHIVE_FILE_NAME: 'archive.json',
+  STORAGE_FILE_NAME: 'storage.json',
+};
